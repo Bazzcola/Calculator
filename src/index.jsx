49842747d31ed21a35eb67be6d4289735815e3ee
DELINSKI ROMAN <Wendigo91@inbox.ru>
2020-07-06 10:35:20 +0300

    fix dev

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
-import {Calculator} from './molecules/Calculator';
+import './index.css';
+import { Calculator } from './molecules/Calculator';
 
 ReactDOM.render(
   <React.StrictMode>
     <Calculator />
   </React.StrictMode>,
-document.getElementById('root')
+  document.getElementById('root')
 );
